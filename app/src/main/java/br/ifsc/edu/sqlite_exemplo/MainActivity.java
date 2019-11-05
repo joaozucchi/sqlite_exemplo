cbf575c29de1405e7c06b65730c3dbc1a274b911
joao <joaohercilio@hotmail.com>
2019-11-05 11:15:59 -0200

    commit inicial

---
+++ app/src/main/java/br/ifsc/edu/sqlite_exemplo/MainActivity.java
@@ -0,0 +1,68 @@
+package br.ifsc.edu.sqlite_exemplo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    SQLiteDatabase bd;
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bd = openOrCreateDatabase("meubd",MODE_PRIVATE,null);
+        bd.execSQL("CREATE TABLE IF NOT EXISTS notas (" +
+                "id integer primary key autoincrement," +
+                "titulo varchar not null," +
+                "texto varchar not null  )");
+
+        //bd.execSQL("INSERT INTO notas (titulo, texto) VALUES ('bah guri','Firebase é melhor') ");
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("titulo", "bah pia");
+        contentValues.put("texto", "Firestore é melhor");
+
+        bd.insert("notas", null, contentValues);
+
+
+        Cursor cursor= bd.rawQuery("SELECT * FROM notas",null,null);
+        cursor.moveToFirst();
+
+        String id;
+        String titulo;
+        String texto;
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        while(!cursor.isAfterLast()){
+
+             //id = cursor.getString(cursor.getColumnIndex("id"));
+             titulo= cursor.getString(cursor.getColumnIndex("titulo"));
+             //texto = cursor.getString(cursor.getColumnIndex("texto"));
+
+            //Log.d("Tabela notas", id +" "+ titulo +" "+ texto);
+            arrayList.add(titulo);
+            cursor.moveToNext();
+        }
+
+        listView = findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                arrayList
+        );
+
+        listView.setAdapter(adapter);
+    }
+}
